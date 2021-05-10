44d66c43b2982a8281a6d616dea40b8bcbc4e46f
Dora <dorababu9008@gmail.com>
2021-05-10 17:01:40 +0530

    new logic added to TestGit

--- src/com/company/TestGit.java
+++ src/com/company/TestGit.java
@@ -1,8 +1,17 @@
 package com.company;
 
 public class TestGit {
+    static int a = 0;
 
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        // a=10;
+        if (a < 10) {
+            System.out.println("Print Zero");
+        } else {
+            System.out.println(a);
+            // a--;
+        }
+
     }
 }
